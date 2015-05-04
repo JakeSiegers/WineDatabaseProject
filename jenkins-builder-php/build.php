870a9e8437787be5b50dc223d814cb2257a06479
Jake Siegers <sirtopeia@yahoo.com>
2015-05-04 09:51:25 -0500

    updates

--- jenkins-builder-php/build.php
+++ jenkins-builder-php/build.php
@@ -10,6 +10,7 @@
 				"--exclude=jenkins-builder-php/",
 				"--exclude=project-files/",
 				"--exclude=.gitignore",
+				"--exclude=db.php",
 				"--exclude=.git/",
 				".",
 				//I assume this won't change. If it does, the build will stop. I feel better putting a absolute URL instead of relative one.
